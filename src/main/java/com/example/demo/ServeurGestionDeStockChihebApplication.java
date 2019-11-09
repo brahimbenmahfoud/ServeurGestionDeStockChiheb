8ec7e4f5673f5b38468db8593122b2582945cc59
brahimbenmahfoud <ibrahimbenmahfoudh@gmail.com>
2019-11-09 09:50:05 +0100

    first cimmit

---
+++ src/main/java/com/example/demo/ServeurGestionDeStockChihebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionDeStockChihebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionDeStockChihebApplication.class, args);
+	}
+
+}
